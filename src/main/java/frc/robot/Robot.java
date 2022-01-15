854fc5e0e2a791d428fdc1026fa9989288ca2f5e
marcuszuccaro <59516978+marcuszuccaro@users.noreply.github.com>
2022-01-15 12:41:02 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drive;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,6 +19,8 @@ import edu.wpi.first.wpilibj2.command.Command;
  * project.
  */
 public class Robot extends TimedRobot {
+	public static Drive drive;
+
 	private Command m_autonomousCommand;
 	private RobotContainer m_robotContainer;
 

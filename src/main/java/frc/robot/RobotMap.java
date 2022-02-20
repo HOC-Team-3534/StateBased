f47fc919f8d84eaeafd4c23e9b08ef225b04b06c
marcuszuccaro <59516978+marcuszuccaro@users.noreply.github.com>
2022-02-19 19:09:15 -0500

    fixed PCM (again?)

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -152,7 +152,8 @@ public class RobotMap {
 
 		m_intakeRoller = new WPI_TalonSRX(Constants.INTAKE_ROLLER);
 		m_mainPCM = new PneumaticsControlModule(Constants.MAIN_PCM);
-		m_intakeKickers = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.INTAKE_EXTEND, Constants.INTAKE_RETRACT);
+
+		m_intakeKickers = m_mainPCM.makeDoubleSolenoid(Constants.INTAKE_EXTEND, Constants.INTAKE_RETRACT);
 
 		//m_compressor = new Compressor(Constants.AIR_COMPRESSOR, PneumaticsModuleType.CTREPCM);
 		

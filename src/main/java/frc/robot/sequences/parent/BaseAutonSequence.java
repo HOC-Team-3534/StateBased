411019aea6ef4b47d0a7a6bdc6209ccc7b80c57f
Malachi Bergman <malachijb28@gmail.com>
2022-02-28 19:23:51 -0500

    add pathplanner follower to baseautonsequence

--- src/main/java/frc/robot/sequences/parent/BaseAutonSequence.java
+++ src/main/java/frc/robot/sequences/parent/BaseAutonSequence.java
@@ -1,10 +1,21 @@
 package frc.robot.sequences.parent;
 
+import frc.robot.sequences.pathplannerfollower.PathPlannerFollower;
+
 public abstract class BaseAutonSequence<S extends IState> extends BaseSequence<S>{
+
+    PathPlannerFollower pathPlannerFollower;
 
     public BaseAutonSequence(S neutralState, S startState) {
         super(neutralState, startState);
         //TODO Auto-generated constructor stub
     }
 
+    void createPathPlannerFollower(String pathName){
+        this.pathPlannerFollower = new PathPlannerFollower(pathName);
+    }
+
+    PathPlannerFollower getPlannerFollower(){
+        return pathPlannerFollower;
+    }
 }

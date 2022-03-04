6c6d9823e83fd1a2e594784f5887a09be559dea5
Malachi Bergman <malachijb28@gmail.com>
2022-03-03 19:37:47 -0500

    oops not all staged :distance funciton interface

--- src/main/java/frc/robot/ILimelightAngleToDistanceFunction.java
+++ src/main/java/frc/robot/ILimelightAngleToDistanceFunction.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public interface ILimelightAngleToDistanceFunction {
+package frc.robot;
+
+public interface ILimelightAngleToDistanceFunction {
+
+    double getDistance(double ty);
+
 }
--- src/main/java/frc/robot/Limelight.java
+++ src/main/java/frc/robot/Limelight.java
@@ -4,12 +4,16 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Limelight {
+
+    ILimelightAngleToDistanceFunction distanceFunction;
+    NetworkTable table;
     
-    public Limelight(){
+    public Limelight(ILimelightAngleToDistanceFunction distanceFunction){
+
+        this.distanceFunction = distanceFunction;
+        this.table = NetworkTableInstance.getDefault().getTable("limelight");
 
     }
-
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
 
     public void getTable(){
         table = NetworkTableInstance.getDefault().getTable("limelight");
@@ -38,6 +42,10 @@ public class Limelight {
     public double getSkew(){
         getTable();
         return table.getEntry("ts").getDouble(0.0);
+    }
+
+    public double getDistance(){
+        return distanceFunction.getDistance(getPixelAngle());
     }
 
 }

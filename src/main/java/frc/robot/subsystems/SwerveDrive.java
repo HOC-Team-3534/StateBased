b8ad99b86a8c467d3d47f6bdb1df299538252b84
Malachi Bergman <malachijb28@gmail.com>
2022-03-09 17:44:31 -0500

    fix format of output strings

--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -125,7 +125,7 @@ public class SwerveDrive extends BaseDriveSubsystem {
 					.getVelocitiesAtCurrentState(this.getSwerveDriveOdometry(), this.getGyroHeading());
 
 			Translation2d currentPosition = getSwerveDriveOdometry().getPoseMeters().getTranslation();
-			System.out.println(String.format("Current Odometry [ X: %.2f Y:%.2f ] Heading [ Rot (radians): %.2f ]", currentPosition.getX(), currentPosition.getY(), getGyroHeading()));
+			System.out.println(String.format("Current Odometry [ X: %.2f Y:%.2f ] Heading [ Rot (radians): %.2f ]", currentPosition.getX(), currentPosition.getY(), getGyroHeading().getRadians()));
 			System.out.println("Current Velocity Calculations: " + velocities.toString());
 			//drive(velocities.getXVel(), velocities.getYVel(), velocities.getRotVel(), true);
 	}

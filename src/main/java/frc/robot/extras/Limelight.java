720d4947f20e0cca3100de5023245aff3cd35892
marcuszuccaro <59516978+marcuszuccaro@users.noreply.github.com>
2022-03-25 19:42:11 -0400

    incomplete limelight stuff

--- src/main/java/frc/robot/extras/Limelight.java
+++ src/main/java/frc/robot/extras/Limelight.java
@@ -7,45 +7,64 @@ public class Limelight {
 
     ILimelightAngleToDistanceFunction distanceFunction;
     NetworkTable table;
-    
-    public Limelight(ILimelightAngleToDistanceFunction distanceFunction){
+    boolean isValid;
+    double savedDistance = -999;
+    double savedTX = 0;
+
+    public Limelight(ILimelightAngleToDistanceFunction distanceFunction) {
 
         this.distanceFunction = distanceFunction;
         this.table = NetworkTableInstance.getDefault().getTable("limelight");
 
     }
 
-    public void getTable(){
+    public void getTable() {
         table = NetworkTableInstance.getDefault().getTable("limelight");
     }
 
-    public double getHorOffset(){
+    public double getHorOffset() {
         getTable();
-        return table.getEntry("tx").getDouble(0.0);
+        if (isValid()) {
+            savedTX = table.getEntry("tx").getDouble(0.0);
+        }
+        return savedTX;
     }
 
-    public double getPixelAngle(){
+    public double getPixelAngle() {
         getTable();
         return table.getEntry("ty").getDouble(0.0);
     }
 
-    public double getHeight(){
+    public double getHeight() {
         getTable();
         return table.getEntry("tvert").getDouble(0.0);
     }
 
-    public double getWidth(){
+    public double getWidth() {
         getTable();
         return table.getEntry("thor").getDouble(0.0);
     }
 
-    public double getSkew(){
+    public double getSkew() {
         getTable();
         return table.getEntry("ts").getDouble(0.0);
     }
 
-    public double getDistance(){
-        return distanceFunction.getDistance(getPixelAngle());
+    public double getDistance() {
+        if (isValid()) {
+            savedDistance = distanceFunction.getDistance(getPixelAngle());
+        }
+        return savedDistance;
+    }
+
+    private boolean getValidTarget() {
+        getTable();
+        return table.getEntry("tv").getDouble(0.0) == 1;
+    }
+
+    public boolean isValid() {
+        isValid = Math.abs(getSkew() + 45) > 40 && getValidTarget();
+        return isValid;
     }
 
 }

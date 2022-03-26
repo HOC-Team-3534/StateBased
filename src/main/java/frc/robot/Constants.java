51eea40d5597bcc7678e7f934ce49c3133d99c42
marcuszuccaro <59516978+marcuszuccaro@users.noreply.github.com>
2022-03-25 20:20:33 -0400

    stupid constants robottype

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,7 +20,7 @@ import edu.wpi.first.math.geometry.Translation2d;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-        public static RobotType ROBOTTYPE = RobotType.PBOT;
+        public static RobotType ROBOTTYPE = RobotType.CBOT;
         /**
          * The left-to-right distance between the drivetrain wheels
          *
